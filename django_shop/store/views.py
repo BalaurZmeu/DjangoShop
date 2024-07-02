964ef5a9711172a72fd55220afda81a2ad296b85
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-02 18:07:21 +0300

    Added view and url for update_item, added function getToken to main.html, added function updateUserOrder with fetch to cart.js

--- django_shop/store/views.py
+++ django_shop/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 def store(request):
@@ -41,3 +42,6 @@ def checkout(request):
         'order': order,
     }
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
