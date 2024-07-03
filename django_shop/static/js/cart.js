87a6f86cd140d076ac06de35868574989e85f613
BalaurZmeu <balaur.zmeu@gmail.com>
2024-07-03 17:47:47 +0300

    Added functionality to change quantity arrow buttons in the cart template

--- django_shop/static/js/cart.js
+++ django_shop/static/js/cart.js
@@ -26,12 +26,13 @@ function updateUserOrder(productId, action){
             'Content-Type':'application/json',
             'X-CSRFToken':csrftoken,
         },
-        body:JSON.stringify({'productId:': productId, 'Action:': action})
+        body:JSON.stringify({'productId': productId, 'action': action})
     })
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         console.log('Data:', data)
+        location.reload()
     });
 }
--- django_shop/store/models.py
+++ django_shop/store/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
